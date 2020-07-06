697ce1b6750620f6a5feaa5d1cf46317d9dd57d7
BrunoMorales <bruno.j.morales@hotmail.com>
2020-07-06 20:04:02 -0300

    created navbar

--- mercadolibre/src/App.tsx
+++ mercadolibre/src/App.tsx
@@ -1,14 +1,12 @@
-import React, { FunctionComponent, ReactElement, useState } from 'react';
+import React, { FunctionComponent, ReactElement } from 'react';
 import NavBar from './components/NavBar/NavBar';
 import './App.css';
 
 const App: FunctionComponent = (): ReactElement => {
-  const [searchInput, setSearchInput] = useState<string>('')
-
   return (
-    <div className="App">
-      <NavBar onType={setSearchInput} />
-    </div>
+    <main className="App">
+      <NavBar />
+    </main>
   );
 }
 
--- mercadolibre/src/components/NavBar/NavBar.tsx
+++ mercadolibre/src/components/NavBar/NavBar.tsx
@@ -1,13 +1,24 @@
 import React, { FunctionComponent, ReactElement } from "react";
+import './NavBar.scss'
 
 interface navBarProps {
-    onType: (searchInput: string) => void
 }
 
-const NavBar: FunctionComponent<navBarProps> = ({ onType }): ReactElement => {
+const NavBar: FunctionComponent<navBarProps> = (): ReactElement => {
+
 
     return (
-        <div />
+        <nav className='navbar'>
+            <div className='navbar-wrapper'>
+                <a href='/' >
+                    <img src='https://http2.mlstatic.com/frontend-assets/ui-navigation/5.6.0/mercadolibre/logo__small.png' alt='' className='nav-logo' />
+                </a>
+                <div className='search-wrapper'>
+                    <input className='search-box' placeholder='Nunca dejes de buscar' />
+                    <button type='submit' className='search-button' />
+                </div>
+            </div>
+        </nav>
     )
 }
 
