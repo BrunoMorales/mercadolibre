c2527ecb2c9a79249fd112338bbd6c8395cbf0e1
BrunoMorales <bruno.j.morales@hotmail.com>
2020-07-09 16:19:30 -0300

    styled

--- mercadolibre/src/components/Result/Result.tsx
+++ mercadolibre/src/components/Result/Result.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, ReactElement } from "react";
 import { result } from "../../utils/fetchItems"
 import './Result.scss'
+import { Link } from "react-router-dom";
 
 interface resultProps {
     data: result
@@ -8,26 +9,29 @@ interface resultProps {
 
 const Result: FunctionComponent<resultProps> = ({ data }): ReactElement => {
     const {
+        id,
         thumbnail,
         price,
         title,
         address: { state_name },
     } = data;
 
-    return <div className='result'>
-        <img src={thumbnail} alt='imagen_articulo' className='thumbnail' />
-        <div>
-            <p>
-                {price}
+    return (
+        <Link className='result' to={`/items/${id}`}>
+            <img src={thumbnail} alt='imagen_articulo' className='thumbnail' />
+            <div>
+                <p className='result-price'>
+                    $ {Math.floor(price)}
+                </p>
+                <h2 className='result-title'>
+                    {title}
+                </h2>
+            </div>
+            <p className='result-location'>
+                {state_name}
             </p>
-            <h2>
-                {title}
-            </h2>
-        </div>
-        <p>
-            {state_name}
-        </p>
-    </div>
+        </Link>
+    )
 }
 
 export default Result
